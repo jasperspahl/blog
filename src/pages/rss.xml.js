ec32cc3aee2f13e1a553c27d3a3a4196cfe771a7
Jasper Spahl <jasperspahl@web.de>
2023-06-01 11:05:03 +0200

    changed side url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ export async function get() {
     return rss({
         title: "jasper's blog",
         description: "My blog about It and Cycling",
-        site: "https://gentle-lokum-9fbb42.netlify.app/",
+        site: "https://jasperspahl.netlify.app/",
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
         customData: `<language>de-de</language>`
     })
